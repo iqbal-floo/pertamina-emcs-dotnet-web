63683c6b11f8f641e229cd43857d63dee74c1144
Iqbal Permana <iqbal@floo.id>
2023-07-31 11:00:29 +0700

    Initial Commit

---
+++ BlazorFullStackCrud/Shared/UnitBisnis.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorFullStackCrud.Shared
+{
+    public class UnitBisnis
+    {
+        public int unit_bisnis_id { get; set; }
+        public string unit_bisnis_kode { get; set; } = string.Empty;
+        public string unit_bisnis_name { get; set; } = string.Empty;
+        public string notes { get; set; } = string.Empty;
+        public bool is_active { get; set; }
+    }
+}
